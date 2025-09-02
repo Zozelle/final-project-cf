840100e8bcf37bc00d5cbad4805a931dba6eab94
Z. Simopoulou <zoesimop123@gmail.com>
2025-09-02 05:10:21 +0300

    installed express-validator and bcrypt, minor change in server port

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ const authRoutes = require('./routes/authRoutes');
 const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware to allow cross-origin requests
 app.use(cors());
